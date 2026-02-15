690cedfdab090ae1d745727ee6dcc4b1e72490b2
diclebahceli <diclebahceli24@gmail.com>
2026-02-15 20:47:12 +0300

    feat: add game_manager logic

--- scripts/path.gd
+++ scripts/path.gd
@@ -1,108 +1,118 @@
+class_name Path
 extends Node2D
+signal path_completed(path: Path2D)
 
 @onready var path_2d: Path2D = $Path2D
 @onready var line_2d: Line2D = $Line2D
 
-var start_planet: Area2D = null
-var current_points: Array[Vector2] = []
+var start_planet : Area2D = null
+var current_points : Array[Vector2] = []
+const MAX_POINTS : float = 200.0
 
-enum PathState {
+enum PathState{
 	IDLE,
 	DRAWING
 }
 
-var state: PathState = PathState.IDLE
+var state : PathState = PathState.IDLE
 
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
-		var mouse_event: InputEventMouseButton = event
+		var mouse_event : InputEventMouseButton = event
 		if mouse_event.button_index == MOUSE_BUTTON_LEFT:
-			var mouse_pos: Vector2 = get_global_mouse_position()
+			var mouse_pos : Vector2 = get_global_mouse_position()
 			if mouse_event.pressed:
 				_on_mouse_pressed(mouse_pos)
-			else:
-				_on_mouse_released(mouse_pos)
-
-
-func _process(_delta: float) -> void:
-	if state != PathState.DRAWING:
-		return
-
-	var mouse_pos: Vector2 = get_global_mouse_position()
-
-	if current_points.is_empty():
-		return
-
-	var last_point: Vector2 = current_points[current_points.size() - 1]
-	if last_point.distance_to(mouse_pos) > 10.0:
-		_add_point(mouse_pos)
-
-
+			else: 
+				_on_mouse_released()
+				
+				
 func _on_mouse_pressed(mouse_pos: Vector2) -> void:
-	var planet: Area2D = _get_planet_under_mouse(mouse_pos)
+	
+	var planet : Area2D = _get_planet_under_point(mouse_pos)
 	if planet == null:
 		return
-
+		
 	start_planet = planet
 	state = PathState.DRAWING
-
+	
 	current_points.clear()
 	line_2d.clear_points()
 	path_2d.curve.clear_points()
-
-	var start_pos: Vector2 = start_planet.global_position
+	
+	var start_pos : Vector2 = start_planet.global_position
 	_add_point(start_pos)
-
-
-func _on_mouse_released(mouse_pos: Vector2) -> void:
+	
+	
+	
+	
+func _on_mouse_released() -> void:
 	if state != PathState.DRAWING:
 		return
-
-	var end_planet: Area2D = _get_planet_under_mouse(mouse_pos)
-
+	var last_point : Vector2 = current_points[current_points.size() - 1]
+	var end_planet : Area2D = _get_planet_under_point(last_point)
 	if end_planet == null or end_planet == start_planet:
 		_cancel_path()
 	else:
 		_complete_path(end_planet)
-
-
+	
+	
+	
 func _cancel_path() -> void:
 	line_2d.clear_points()
 	path_2d.curve.clear_points()
 	current_points.clear()
-
+	
 	start_planet = null
 	state = PathState.IDLE
-
-
-func _complete_path(end_planet: Area2D) -> void:
+	
+	
+func _complete_path(end_planet : Area2D) -> void:
 	var end_pos: Vector2 = end_planet.global_position
 	_add_point(end_pos)
-
+	
 	start_planet = null
 	state = PathState.IDLE
+	path_completed.emit(path_2d)
 
-
-func _add_point(point: Vector2) -> void:
-				
+	
+func _add_point(point : Vector2) -> void:
+	print(current_points.size())
 	current_points.append(point)
-	print(current_points)
 	line_2d.add_point(point)
 	path_2d.curve.add_point(point)
-
-
-func _get_planet_under_mouse(mouse_pos: Vector2) -> Area2D:
-	var space_state: PhysicsDirectSpaceState2D = get_world_2d().direct_space_state
-
-	var query: PhysicsPointQueryParameters2D = PhysicsPointQueryParameters2D.new()
+	
+	
+	
+func _get_planet_under_point(mouse_pos : Vector2) -> Area2D:
+	var space_state : PhysicsDirectSpaceState2D	 = get_world_2d().direct_space_state
+	
+	var query : PhysicsPointQueryParameters2D = PhysicsPointQueryParameters2D.new()
 	query.position = mouse_pos
 	query.collide_with_areas = true
-
-	var results: Array[Dictionary] = space_state.intersect_point(query)
-	for result: Dictionary in results:
+	
+	var results : Array[Dictionary] = space_state.intersect_point(query)
+	for result : Dictionary in results:
 		var collider: Node2D = result["collider"]
 		if collider is Planet:
 			return collider
-
 	return null
+	
+func _process(_delta:float) -> void:
+	
+	if current_points.size() > MAX_POINTS:
+		return
+	
+	
+	if state != PathState.DRAWING:
+		return
+		
+	var mouse_pos : Vector2 = get_global_mouse_position()
+	
+	if current_points.is_empty():
+		return
+		
+	var last_point : Vector2 = current_points[current_points.size() -1]
+	if last_point.distance_to(mouse_pos) > 10.0:
+		_add_point(mouse_pos)
--- scripts/unit.gd
+++ scripts/unit.gd
@@ -1,20 +1,9 @@
-extends CharacterBody2D
+extends PathFollow2D
+
+const SPEED: float = 900.0
 
 
-const SPEED : float = 9000.0
-@export var start_point : Marker2D
-@export var end_point : Marker2D
-
-
-
-func _physics_process(delta: float) -> void:
-
+func _process(delta: float) -> void:
 	
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction : Vector2 = (end_point.global_position - start_point.global_position).normalized()
-	
-	if direction:
-		velocity = direction * SPEED * delta
-
-	move_and_slide()
+	progress += delta * SPEED 
+		
